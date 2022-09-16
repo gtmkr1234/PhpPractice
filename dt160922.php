fe6f58ee6a9a1f86ed69ee956eb3fb13895f618f
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-09-16 08:49:54 +0530

    changes

--- dt160922.php
+++ dt160922.php
@@ -1,1 +1,19 @@
 <?php
+echo "<table style='border: 2px solid black; border-collapse: collapse; text-align: center;'>";
+for($i=1; $i<=8; $i++){
+    echo "<tr>";
+    for($j=1; $j<=8;$j++){
+        if(($i+$j)%2==0){
+            echo "<td style='border: 1px solid red; color: white; background-color: black; height: 50px; width: 50px;'>";
+            echo $i.",".$j;
+            echo "</td>";
+        }
+        else{
+            echo "<td style='border: 1px solid red; background-color: white; height: 50px; width: 50px;'>";
+            echo $i.",".$j;
+            echo "</td>";
+
+        }
+    }
+    echo "</tr>";
+}
