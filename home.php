3fbff8b3eee548ca929557f0ca7653e04a74a088
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-10-21 09:15:48 +0530

    changes

--- home.php
+++ home.php
@@ -1,15 +1,17 @@
 <?php
-$username = "admin";
+$username = "admin@com";
 $passw = "admin123";
 session_start();
 if(isset($_SESSION['user'])){
 
 }
 else{
-    if(){
+    if($username == $_POST['uname'] and $passw == $_POST['password']){
 
     }
     else{
-
+        echo "Invalid Username or password";
+        echo "<script>alert('Invalid Username or password')</script>";
+        echo "<script>location.href='login.html'</script>";
     }
 }
