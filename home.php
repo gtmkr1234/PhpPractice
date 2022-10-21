aaaaee69274ba2760e7c09114dc0c0c55d79e341
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-10-21 09:25:23 +0530

    changes

--- home.php
+++ home.php
@@ -3,11 +3,16 @@ $username = "admin@com";
 $passw = "admin123";
 session_start();
 if(isset($_SESSION['user'])){
-
+    echo "Welcome"."<h3 style='color: red align: right'>".$_SESSION['user']."</h3>";
+    echo "You are scrolling home page";
+    echo "<br>";
+    echo "<a href='product.php'> Click here to navigate to the product page ";
+    echo "<a href='logout.php'><input type ='submit' name='logout'></a> ";
 }
 else{
     if($username == $_POST['uname'] and $passw == $_POST['password']){
-
+        $_SESSION['user'] = $username;
+        echo "<script>location.href='home.php'</script>";
     }
     else{
         echo "Invalid Username or password";
