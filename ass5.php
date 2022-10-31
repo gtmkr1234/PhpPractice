ceeb736271011a60734ecc208b21d3ba7dc5a040
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-10-31 22:05:12 +0530

    changes

---
+++ ass5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        <table>
+            <tr>
+                <td>Guess a number between 1-100:</td>
+                <td><input type="text" name="numguess"></td>
+                <td><input type="submit" value="check"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
