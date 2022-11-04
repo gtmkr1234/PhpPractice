38d87f2bff702b3183d6dd82b6da6a819c308c6e
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-11-04 08:51:42 +0530

    changes

--- Classes and Objects/class1.php
+++ Classes and Objects/class1.php
@@ -2,16 +2,25 @@
 class test{
     public int $sum = 45;
     private int $count = 54;
+    public function hello(){
+        echo "Welcome"."<br>";
+        echo $this->sum;
+    }
 }
 $obj_test = new test();
 $test_2 = new test;
 var_dump($obj_test);
 echo "<br>";
-echo $obj_test->sum;
+echo $obj_test->sum."<br>";
 /*
  * Output of this snippet :-
  * object(test)#1 (2) { ["sum"]=> int(45) ["count":"test":private]=> int(54) }
  * 45
  */
 //echo $obj_test->count; can not access because getter and setter methods are required.
-
+echo $obj_test->hello();
+/*
+ * Output of this snippet:-
+ * Welcome
+ * 45
+ */
