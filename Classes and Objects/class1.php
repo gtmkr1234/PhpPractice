99a245fe48a8a0d04cac594bad958cbef1652395
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-11-04 08:22:41 +0530

    changes

--- Classes and Objects/class1.php
+++ Classes and Objects/class1.php
@@ -1,10 +1,11 @@
 <?php
 class test{
-    public $sum = 45;
-    private $count = 45;
+    public int $sum = 45;
+    private int $count = 45;
+
 }
 $obj_test = new test();
 $test_2 = new test;
 echo $obj_test;
 
-?>
+
