aaffcc97d88fd07a79d8ad8c46bd0a2795b5fff4
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-11-14 12:35:13 +0530

    file handling functions

--- File Handling/fh5.php
+++ File Handling/fh5.php
@@ -7,3 +7,12 @@ while(!feof($handle)){
     }
     echo $data;
 }
+//unlink() -> to delete file from the system
+//copy(source_file, dest_file) -> to make a copy of a file to another file
+//file_exists(filename) -> to check if a file exists or not. Return type - boolean
+if(file_exists("Resume.txt")){
+    echo "<br> File exists";
+}
+else{
+    echo "<br> File doesn't exist";
+}
